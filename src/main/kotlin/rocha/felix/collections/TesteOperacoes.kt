77b7784d268149a532d065de3f71ea0dc8b5547d
RFelixRocha <felixrochaeso@gmail.com>
2021-03-09 23:28:17 -0400

    Interagindo com o tipo de array doubleArray e doubleArrayOf -- operações prédefindas

---
+++ src/main/kotlin/rocha/felix/collections/TesteOperacoes.kt
@@ -0,0 +1,21 @@
+package rocha.felix.collections
+
+fun main(){
+
+    val salarios = doubleArrayOf(1000.0,4000.0,3000.0,2000.0)
+
+    salarios.forEachIndexed{index,valor->
+        println(valor)
+    }
+
+
+    println("==============================")
+    println("Maior Salario: ${salarios.max()}")
+    println("Menor salario ${salarios.min()}")
+    println("Salario Medio: ${salarios.average()}")
+
+    //Seleciona os maiores salarios de acordo com o filtro
+    val salarioFiltro = salarios.filter { it > 2000.0 };
+    println("==============================")
+    salarioFiltro.forEach { println(it) }
+}
