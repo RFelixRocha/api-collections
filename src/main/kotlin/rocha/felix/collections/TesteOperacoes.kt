4cc86ad515d92aebaf8fc6ead1cfad15f7da5c6b
RFelixRocha <felixrochaeso@gmail.com>
2021-03-09 23:33:47 -0400

    Interagindo com o tipo de array doubleArray e doubleArrayOf -- RANGE

--- src/main/kotlin/rocha/felix/collections/TesteOperacoes.kt
+++ src/main/kotlin/rocha/felix/collections/TesteOperacoes.kt
@@ -18,4 +18,7 @@ fun main(){
     val salarioFiltro = salarios.filter { it > 2000.0 };
     println("==============================")
     salarioFiltro.forEach { println(it) }
+
+    println("============RANGE==================")
+    println(salarios.count{it in 1200.0..5000.0})
 }
