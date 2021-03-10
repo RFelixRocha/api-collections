820e17a5a444eaf25a09fe7e12fb2b41df338000
RFelixRocha <felixrochaeso@gmail.com>
2021-03-10 00:03:20 -0400

    Ordenando lista  -- groupBy

--- src/main/kotlin/rocha/felix/collections/TesteList.kt
+++ src/main/kotlin/rocha/felix/collections/TesteList.kt
@@ -1,11 +1,13 @@
 package rocha.felix.collections
 
 fun main() {
-    val maria = Funcionarios("Maria", 678.90)
-    val felix = Funcionarios("Felix", 6000.90)
-    val larissa = Funcionarios("Larissa", 1500.90)
+    val maria = Funcionarios("Maria", 678.90,"CLT")
+    val felix = Funcionarios("Felix", 6000.90,"Autonomo")
+    val larissa = Funcionarios("Larissa", 1500.90,"CLT")
+    val vanessa = Funcionarios("Vanessa", 1500.90,"CLT")
+    val joana = Funcionarios("Joana", 1500.90,"CLT")
 
-    val listFuncionarios = listOf(maria, felix, larissa)
+    val listFuncionarios = listOf(maria, felix, larissa,vanessa)
 
     listFuncionarios.forEach { println(it) }
 
@@ -14,12 +16,21 @@ fun main() {
     println(listFuncionarios.find { it.nome == "Larissa" })
 
     println("================SORTBY=====================")
-    listFuncionarios.sortedBy { it.salario }.forEach { println(it) }
+    listFuncionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("================GROUPBY=====================")
+    listFuncionarios
+        .groupBy { it.tipo }
+        .forEach{ println(it)}
+
 }
 
 data class Funcionarios(
     val nome :String,
-    val salario :Double
+    val salario :Double,
+    val tipo :String
 ){
     override fun toString(): String  = """
         Nome:    $nome
