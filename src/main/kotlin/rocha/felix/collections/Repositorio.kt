fffe800c47150bd64e78f0fb531b3c60f5fdec7f
RFelixRocha <felixrochaeso@gmail.com>
2021-03-10 19:51:58 -0400

    criando map

--- src/main/kotlin/rocha/felix/collections/Repositorio.kt
+++ src/main/kotlin/rocha/felix/collections/Repositorio.kt
@@ -1,4 +1,12 @@
 package rocha.felix.collections
 
-class Repositorio {
+class Repositorio <T>{
+
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+        map[id] = value
+    }
+
+    fun findById(id: String) = map[id]
 }
